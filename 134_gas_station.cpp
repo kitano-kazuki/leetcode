55b5458867ee831d0790ea9e94f7fcbc12736d46
kazuki <kitano-kazuki954@e.c..u-tokyo.ac.jp>
2024-08-23 13:12:18 +0900

    gas station

---
+++ 134_gas_station.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+      int total_surplus = 0;
+      int surplus = 0;
+      int start = 0;
+      for(int i = 0; i < gas.size(); i++){
+        total_surplus += gas[i] - cost[i];
+        surplus += gas[i] - cost[i];
+        if(surplus < 0){
+          surplus = 0;
+          start = i + 1;
+        }
+      }
+      if(total_surplus >= 0){
+        return start;
+      }
+      return -1;
+        
+    }
+};
