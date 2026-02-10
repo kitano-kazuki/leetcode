aa0fc1836407d62aa9f2bed6e1e0dec9b1f8d5b9
Kazuki Kitano <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2026-02-10 16:36:18 +0900

    step3

---
+++ step3-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+        if not fast or not fast.next:
+            return None
+        while slow != head:
+            head = head.next
+            slow = slow.next
+        return head
