388a3d273ae1dda6b3d1864670ee5d09212aa790
Kazuki Kitano <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2026-02-14 00:14:40 +0900

    step5 2回目

--- step5-3.py
+++ step5-3.py
@@ -8,26 +8,22 @@ class Solution:
         open_brackets = open_to_close.keys()
         processing_idx = 0
 
-        def is_chunk_valid():
+        def check_chunk_valid():
             nonlocal processing_idx
-            if processing_idx >= len(s):
-                return False
             if s[processing_idx] not in open_brackets:
                 return False
             expected_end_bracket = open_to_close[s[processing_idx]]
             processing_idx += 1
             while processing_idx < len(s) and s[processing_idx] != expected_end_bracket:
-                if not is_chunk_valid():
+                if not check_chunk_valid():
                     return False
             if processing_idx >= len(s):
                 return False
             processing_idx += 1
             return True
-
+        
         while processing_idx < len(s):
-            if not is_chunk_valid():
+            if not check_chunk_valid():
                 return False
         
         return True
-                
-                
