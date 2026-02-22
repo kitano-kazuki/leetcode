02f805ec2c3809be1823e1397f4843cd54d8a9fa
Kazuki Kitano <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2026-02-22 17:50:23 +0900

    step3 ２回目

--- step3-2.py
+++ step3-2.py
@@ -1,18 +1,17 @@
 # QuickSelectを使用
 import random
 
-
 class Solution:
     def partition(self, uniques, num_to_count, left, right, pivot_idx):
         pivot = num_to_count[uniques[pivot_idx]]
         uniques[pivot_idx], uniques[right] = uniques[right], uniques[pivot_idx]
-        partition_idx = left
+        partitioned_idx = left
         for i in range(left, right):
             if num_to_count[uniques[i]] <= pivot:
-                uniques[i], uniques[partition_idx] = uniques[partition_idx], uniques[i]
-                partition_idx += 1
-        uniques[partition_idx], uniques[right] = uniques[right], uniques[partition_idx]
-        return partition_idx
+                uniques[i], uniques[partitioned_idx] = uniques[partitioned_idx], uniques[i]
+                partitioned_idx += 1
+        uniques[partitioned_idx], uniques[right] = uniques[right], uniques[partitioned_idx]
+        return partitioned_idx
 
     def quick_select(self, uniques, num_to_count, left, right, smallest_k):
         if left == right:
@@ -26,14 +25,15 @@ class Solution:
             return
         self.quick_select(uniques, num_to_count, partitioned_idx + 1, right, smallest_k)
         return
-        
+
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_to_count = {}
+        num_to_counts = {}
         for num in nums:
-            num_to_count.setdefault(num, 0)
-            num_to_count[num] += 1
-        n = len(num_to_count)
-        uniques = list(num_to_count)
-        self.quick_select(uniques, num_to_count, 0, n - 1, n - k)
+            num_to_counts.setdefault(num, 0)
+            num_to_counts[num] += 1
+        n = len(num_to_counts)
+        uniques = list(num_to_counts)
+        self.quick_select(uniques, num_to_counts, 0, n - 1, n - k)
         return uniques[n - k:]
         
