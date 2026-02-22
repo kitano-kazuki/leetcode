cf22a13560d490c229e827a8069a1d02f53c9b7b
Kazuki Kitano <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2026-02-22 18:00:00 +0900

    step3 3回目

--- step3-1.py
+++ step3-1.py
@@ -3,16 +3,16 @@ import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_to_counts = {}
+        num_to_count = {}
         for num in nums:
-            num_to_counts.setdefault(num, 0)
-            num_to_counts[num] += 1
+            num_to_count.setdefault(num, 0)
+            num_to_count[num] += 1
         topk_heap = []
-        for num, count in num_to_counts.items():
+        for num, count in num_to_count.items():
             if len(topk_heap) < k:
                 heapq.heappush(topk_heap, (count, num))
                 continue
             if count <= topk_heap[0][0]:
                 continue
             heapq.heappushpop(topk_heap, (count, num))
-        return [num for _, num in topk_heap]
+        return [num for count, num in topk_heap]
--- step3-2.py
+++ step3-2.py
@@ -2,38 +2,39 @@
 import random
 
 class Solution:
-    def partition(self, uniques, num_to_count, left, right, pivot_idx):
-        pivot = num_to_count[uniques[pivot_idx]]
-        uniques[pivot_idx], uniques[right] = uniques[right], uniques[pivot_idx]
+    def partition(self, unique_nums, num_to_count, left, right, pivot_idx):
+        pivot = num_to_count[unique_nums[pivot_idx]]
+        unique_nums[pivot_idx], unique_nums[right] = unique_nums[right], unique_nums[pivot_idx]
         partitioned_idx = left
         for i in range(left, right):
-            if num_to_count[uniques[i]] <= pivot:
-                uniques[i], uniques[partitioned_idx] = uniques[partitioned_idx], uniques[i]
+            if num_to_count[unique_nums[i]] <= pivot:
+                unique_nums[i], unique_nums[partitioned_idx] = unique_nums[partitioned_idx], unique_nums[i]
                 partitioned_idx += 1
-        uniques[partitioned_idx], uniques[right] = uniques[right], uniques[partitioned_idx]
+        unique_nums[partitioned_idx], unique_nums[right] = unique_nums[right], unique_nums[partitioned_idx]
         return partitioned_idx
+        
 
-    def quick_select(self, uniques, num_to_count, left, right, smallest_k):
+    def quick_select(self, unique_nums, num_to_count, left, right, smallest_k):
         if left == right:
             return
         pivot_idx = random.randint(left, right)
-        partitioned_idx = self.partition(uniques, num_to_count, left, right, pivot_idx)
+        partitioned_idx = self.partition(unique_nums, num_to_count, left, right, pivot_idx)
         if partitioned_idx == smallest_k:
             return
         if partitioned_idx > smallest_k:
-            self.quick_select(uniques, num_to_count, left, partitioned_idx - 1, smallest_k)
+            self.quick_select(unique_nums, num_to_count, left, partitioned_idx - 1, smallest_k)
             return
-        self.quick_select(uniques, num_to_count, partitioned_idx + 1, right, smallest_k)
+        self.quick_select(unique_nums, num_to_count, partitioned_idx + 1, right, smallest_k)
         return
 
-
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_to_counts = {}
+        num_to_count = {}
         for num in nums:
-            num_to_counts.setdefault(num, 0)
-            num_to_counts[num] += 1
-        n = len(num_to_counts)
-        uniques = list(num_to_counts)
-        self.quick_select(uniques, num_to_counts, 0, n - 1, n - k)
-        return uniques[n - k:]
+            num_to_count.setdefault(num, 0)
+            num_to_count[num] += 1
+        unique_nums = list(num_to_count)
+        n = len(unique_nums)
+        self.quick_select(unique_nums, num_to_count, 0, n - 1, n - k)
+        return unique_nums[n - k:]
+
         
