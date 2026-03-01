4fa6961c2fca87c940674ddac127b82dbaa4e15e
Kazuki Kitano <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2026-03-01 19:15:43 +0900

    step3 3回め

--- step3-1.py
+++ step3-1.py
@@ -3,10 +3,9 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_word_to_group = {}
+        sorted_word_to_count = {}
         for word in strs:
             sorted_word = "".join(sorted(word))
-            sorted_word_to_group.setdefault(sorted_word, [])
-            sorted_word_to_group[sorted_word].append(word)
-        return list(sorted_word_to_group.values())
-
+            sorted_word_to_count.setdefault(sorted_word, [])
+            sorted_word_to_count[sorted_word].append(word)
+        return list(sorted_word_to_count.values())
