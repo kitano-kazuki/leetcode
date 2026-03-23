ba33f9d7955d7ce505039e0f16f7c6aeef709b40
Kazuki Kitano <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2026-03-24 04:30:16 +0900

    step3 1st

---
+++ step3-2_dfs.py
@@ -0,0 +1,25 @@
+# 1st: 3:27
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode | None, targetSum: int) -> bool:
+        if root is None:
+            return False
+        node_to_visit = [(root, targetSum)]
+        while node_to_visit:
+            node, target_sum_at_this_node = node_to_visit.pop()
+            is_leaf = node.left is None and node.right is None
+            if is_leaf and node.val == target_sum_at_this_node:
+                return True
+            if node.left is not None:
+                node_to_visit.append((node.left, target_sum_at_this_node - node.val))
+            if node.right is not None:
+                node_to_visit.append((node.right, target_sum_at_this_node - node.val))
+        return False
+        
