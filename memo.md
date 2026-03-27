fa6cc91dea02c91214cd0f5c367c2862f2faabc6
Kazuki Kitano <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2026-03-27 14:47:03 +0900

    step3 memo

--- memo.md
+++ memo.md
@@ -220,3 +220,31 @@ class Solution:
 
 
 # Step3
+
+```python
+# 1st: 1:29
+# 2nd: 1:32
+# 3rd: 1:16
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        
+        node_and_valid_bounds = [(root, float("-inf"), float("inf"))]
+        while node_and_valid_bounds:
+            node, lower_bound_exclusive, upper_bound_exclusive = node_and_valid_bounds.pop()
+            if not (lower_bound_exclusive < node.val < upper_bound_exclusive):
+                return False
+            if node.left is not None:
+                node_and_valid_bounds.append((node.left, lower_bound_exclusive, node.val))
+            if node.right is not None:
+                node_and_valid_bounds.append((node.right, node.val, upper_bound_exclusive))
+        return True
+```
--- step3-2_dfs.py
+++ step3-2_dfs.py
@@ -1,4 +1,6 @@
 # 1st: 1:29
+# 2nd: 1:32
+# 3rd: 1:16
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -10,15 +12,14 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         if root is None:
             return True
-
-        node_and_valid_range_pairs = [(root, float("-inf"), float("inf"))]
-        while node_and_valid_range_pairs:
-            node, lower_bound_exclusive, upper_bound_exclusive = node_and_valid_range_pairs.pop()
+        
+        node_and_valid_bounds = [(root, float("-inf"), float("inf"))]
+        while node_and_valid_bounds:
+            node, lower_bound_exclusive, upper_bound_exclusive = node_and_valid_bounds.pop()
             if not (lower_bound_exclusive < node.val < upper_bound_exclusive):
                 return False
             if node.left is not None:
-                node_and_valid_range_pairs.append((node.left, lower_bound_exclusive, node.val))
+                node_and_valid_bounds.append((node.left, lower_bound_exclusive, node.val))
             if node.right is not None:
-                node_and_valid_range_pairs.append((node.right, node.val, upper_bound_exclusive))
+                node_and_valid_bounds.append((node.right, node.val, upper_bound_exclusive))
         return True
-        
