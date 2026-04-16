4e33fdd103e6a405529e14d96e4051e407fb3956
Kazuki Kitano <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2026-04-16 10:39:34 +0900

    step3 2nd

--- step3-1_dp.py
+++ step3-1_dp.py
@@ -1,10 +1,11 @@
 # 1st: 1:08
+# 2st: 1:00
 
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        max_subarray_sum = float("-inf")
-        max_prefix_sum_ending_before = 0
+    def maxSubArray(self, nums: list[int]) -> int:
+        max_subarray = float("-inf")
+        max_subarray_ending_before = 0
         for num in nums:
-            max_prefix_sum_ending_before = max(max_prefix_sum_ending_before + num, num)
-            max_subarray_sum = max(max_subarray_sum, max_prefix_sum_ending_before)
-        return max_subarray_sum
+            max_subarray_ending_before = max(max_subarray_ending_before + num, num)
+            max_subarray = max(max_subarray, max_subarray_ending_before)
+        return max_subarray
--- step3-2_divide_and_conquer.py
+++ step3-2_divide_and_conquer.py
@@ -1,34 +1,36 @@
 # 1st: 8:10
+# 2nd: 6:50
 
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        return self.max_sub_array_in_range(nums, 0, len(nums) - 1)
+    def maxSubArray(self, nums: list[int]) -> int:
+        return self.max_subarray_in_range(nums, 0, len(nums) - 1)
 
-    def max_sub_array_in_range(self, nums, left, right) -> int:
+    def max_subarray_in_range(self, nums, left, right) -> int:
         if left == right:
             return nums[left]
 
-        def max_subarray_in_range_through_middle(nums, left, right, mid) -> int:
-            leftward_max_subarray = float("-inf")
+        def max_subarray_in_range_through_mid(nums, left, right, mid) -> int:
+            leftward_max_sum = float("-inf")
             leftward_sum = 0
             for i in range(mid, left - 1, -1):
                 leftward_sum += nums[i]
-                leftward_max_subarray = max(leftward_max_subarray, leftward_sum)
-
-            rightward_max_subarray = 0
+                leftward_max_sum = max(leftward_max_sum, leftward_sum)
+            
+            rightward_max_sum = 0
             rightward_sum = 0
             for i in range(mid + 1, right + 1, 1):
                 rightward_sum += nums[i]
-                rightward_max_subarray = max(rightward_max_subarray, rightward_sum)
+                rightward_max_sum = max(rightward_max_sum, rightward_sum)
             
-            return leftward_max_subarray + rightward_max_subarray
-
+            return leftward_max_sum + rightward_max_sum
+        
         mid = (left + right) // 2
-        max_sub_array_in_left_half = self.max_sub_array_in_range(nums, left, mid)
-        max_sub_array_in_right_half = self.max_sub_array_in_range(nums, mid + 1, right)
-        max_sub_array_through_middle = max_subarray_in_range_through_middle(nums, left, right, mid)
+        left_max_subarray = self.max_subarray_in_range(nums, left, mid)
+        right_max_subarray = self.max_subarray_in_range(nums, mid + 1, right)
+        through_mid_max_subarray = max_subarray_in_range_through_mid(nums, left, right, mid)
+
         return max(
-            max_sub_array_in_left_half,
-            max_sub_array_in_right_half,
-            max_sub_array_through_middle
+            left_max_subarray,
+            right_max_subarray,
+            through_mid_max_subarray
         )
