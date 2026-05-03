9330b149bb7f09d905bbe5e7e5fb15b61b0ca5a9
Kazuki Kitano <kitano-kazuki954@g.ecc.u-tokyo.ac.jp>
2026-05-03 15:02:47 +0900

    step3 23

--- code3-1.py
+++ code3-1.py
@@ -7,26 +7,25 @@ class Solution:
 
         empty_rooms = list(range(n))
         heapq.heapify(empty_rooms)
-        release_time_and_room = []
         postponed_meeting_durations = collections.deque([])
+        release_time_and_room = []
         room_usage_count = [0] * n
 
-        def release_and_reserve_by_endtime(endtime) -> None:
+        def release_and_reserve_by_endtime(endtime):
             while release_time_and_room and release_time_and_room[0][0] <= endtime:
-                # 部屋を解放
-                released_time, released_room = heapq.heappop(release_time_and_room)
+                # 部屋を開放
+                releaed_time, released_room = heapq.heappop(release_time_and_room)
                 heapq.heappush(empty_rooms, released_room)
 
-                # 部屋を確保
+                # 部屋を予約
                 if postponed_meeting_durations:
-                    room = heapq.heappop(empty_rooms)
                     postponed_meeting_duration = postponed_meeting_durations.popleft()
-                    heapq.heappush(release_time_and_room, 
-                                  (released_time + postponed_meeting_duration, room))
+                    room = heapq.heappop(empty_rooms)
+                    heapq.heappush(release_time_and_room, (releaed_time + postponed_meeting_duration, room))
                     room_usage_count[room] += 1
             return
 
-        for start, end in sorted(meetings, key=lambda x: x[0]):
+        for start, end in sorted(meetings, key=lambda x : x[0]):
             release_and_reserve_by_endtime(start)
 
             if not empty_rooms:
@@ -37,8 +36,6 @@ class Solution:
                 room_usage_count[room] += 1
 
         release_and_reserve_by_endtime(float("inf"))
-
+        
         max_usage = max(room_usage_count)
         return room_usage_count.index(max_usage)
-        
-        
